c941cce72657d871d7714676b1d3e4d0250d951e
Nika S <sleep.tight@post.com>
2016-12-09 10:59:39 +0100

    #2 imported models to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Series, Book
+
+admin.site.register(Author)
+admin.site.register(Series)
+admin.site.register(Book)
