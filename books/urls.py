ac3b12efd68e397adc66f45edba2c80b1cd82262
Nika S <sleep.tight@post.com>
2017-01-20 12:10:30 +0100

    #11 book details page

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # ex: /books/
     url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /books/5/
-#    u<rl(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /books/5/results/
     #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /books/5/vote/
--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-#from django.urls import reverse
+from django.urls import reverse
 from django.views import generic
 
 from .models import Book
@@ -11,5 +11,16 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_books_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        """Return the last five published books."""
         return Book.objects.order_by('-created_at')[:5]
+    
+
+class DetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/detail.djt'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Book.objects
