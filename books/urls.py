329313451e5c8c93235bb211efc1a2fa65f44156
Nika S <sleep.tight@post.com>
2017-01-27 10:52:14 +0100

    #14 - fixed urls

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /books/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # ex: /books/author/5/
+    url(r'^author/(?P<pk>[0-9]+)/$', views.AuthorView.as_view(), name='author_detail'),
     # ex: /books/5/results/
     #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /books/5/vote/
