4d75b5de3f07185f4542911e02e587788fe7378f
Nika S <sleep.tight@post.com>
2017-03-06 10:42:34 +0100

    #18 - added shelf actions select

--- shelf/models.py
+++ shelf/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from books.models import Book
 
+ACTION_CHOICES = (
+    ('liked', 'Liked'),
+    ('added', 'Added'),
+    ('read', 'Read'),
+    ('have', 'Have'),
+)
+
 class Shelf(models.Model):
     user = models.ForeignKey(User, unique=False)
     book = models.ForeignKey(Book, unique=False)
-    action = models.CharField(max_length=200)
+    action = models.CharField(max_length=200, choices=ACTION_CHOICES)
     date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField('date published', auto_now_add=True)
     updated_at = models.DateTimeField('date updated', auto_now_add=True)
