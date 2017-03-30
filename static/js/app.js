b5f4f8515cd643d7ca6f4924fce3d0099d81ef88
Nika S <sleep.tight@post.com>
2017-03-30 13:58:40 +0200

    #19 added notifications after action

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,11 @@
+function showAlert(message, type = 'success') {
+    $("#alert_placeholder").html('<div class="alert alert-' + type + '">' + message + '</div>');
+    alertTimeout(3000);
+}
+
+function alertTimeout(wait) {
+    setTimeout(function () {
+        $('#alert_placeholder').children('.alert:first-child').remove();
+    }, wait);
+}
 
