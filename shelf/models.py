16bbcc9130ef641c88f5adb4f110e54b6035ffe0
Nika S <sleep.tight@post.com>
2017-03-31 14:58:06 +0200

    #18 - changed choices from tuple to dictionary

--- shelf/models.py
+++ shelf/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from books.models import Book
 
-ACTION_CHOICES = (
-    ('liked', 'Liked'),
-    ('added', 'Added'),
-    ('read', 'Read'),
-    ('have', 'Have'),
-)
+ACTION_CHOICES = {
+    'want-to-read': 'Want to read',
+    'currently-reading': 'Currently reading',
+    'read': 'Read',
+    'like': 'Like',
+    'have': 'Have',  
+}
 
 class Shelf(models.Model):
     user = models.ForeignKey(User, unique=False)
     book = models.ForeignKey(Book, unique=False)
-    action = models.CharField(max_length=200, choices=ACTION_CHOICES)
+    action = models.CharField(max_length=200, choices=ACTION_CHOICES.items())
     date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField('date published', auto_now_add=True)
     updated_at = models.DateTimeField('date updated', auto_now_add=True)
