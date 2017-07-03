3ef01f448c965a032a634670fcd83be1c8e44c4c
Nika S <sleep.tight@post.com>
2017-07-03 14:12:01 +0200

    #16 - added language and translator

--- books/models.py
+++ books/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+LANGUAGES = {
+    'en': 'English',
+    'ru': 'Русский',
+    'de': 'German',
+    'pl': 'Polish',
+    'fr': 'French',  
+}
+
 class Author(models.Model):
     name = models.CharField(max_length=200)
     name_orig = models.CharField(max_length=200)
@@ -11,6 +19,17 @@ class Author(models.Model):
     photo = models.ImageField(upload_to='authors', null=True, blank=True)
     pass
 
+    def __str__(self):
+        return "{} {}".format(self.name, self.surname)
+    
+class Translator(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    birth_date = models.DateField(null=True, blank=True)
+    biography = models.TextField(blank=True)
+    created_at = models.DateTimeField('date published', auto_now_add=True)
+    photo = models.ImageField(upload_to='authors', null=True, blank=True)
+    
     def __str__(self):
         return "{} {}".format(self.name, self.surname)
 
@@ -42,6 +61,7 @@ class Form(models.Model):
 
 class Book(models.Model):
     authors = models.ManyToManyField(Author, blank=True, related_name='books')
+    translators = models.ManyToManyField(Translator, blank=True, related_name='books')
     series = models.ManyToManyField(Series, blank=True, related_name='books')
     genres = models.ManyToManyField(Genre, blank=True, related_name='books')
     form = models.ForeignKey(Form, null=True, unique=False)
@@ -49,6 +69,7 @@ class Book(models.Model):
     title_orig = models.CharField(max_length=255)
     teaser = models.CharField(max_length=500, blank=True)
     description = models.TextField(blank=True)
+    language = models.CharField(max_length=2, default='en', choices=LANGUAGES.items())
     pub_date = models.DateField(null=True, blank=True)
     isbn = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField('date published', auto_now_add=True)
