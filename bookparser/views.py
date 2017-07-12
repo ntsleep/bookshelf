8fed05a2b4e4be421333748731d4042584736c7d
Nika S <sleep.tight@post.com>
2017-07-12 13:23:59 +0200

    #29 - parsed fantlab author page

--- bookparser/views.py
+++ bookparser/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import get_object_or_404, render, render_to_response
 from django.views import generic
 from django.http import JsonResponse
 from bs4 import BeautifulSoup
-import re
 from urllib.request import urlopen
+from datetime import datetime
+
+import re
+import locale
 
 
 class IndexView(generic.ListView):
@@ -15,10 +18,12 @@ def parse_url(request):
     """Getting title, encoding, H1 after timeshift"""
 
 #    url = "https://fantlab.ru/work2654" #Neuromancer
-    url = "https://fantlab.ru/work4065" #Роберт Асприн, Линда Эванс «Разведчики времени»
+#    url = "https://fantlab.ru/work4065" #Роберт Асприн, Линда Эванс «Разведчики времени»
+
+    url = "https://fantlab.ru/autor111" #Уильям Гибсон (William Gibson)
 
     # Getting main data from the page
-    success, encoding, book = fetch_book_data(url)
+    success, encoding, book = fetch_author_data(url)
 
 
     
@@ -71,6 +76,54 @@ def fetch_book_data(url):
             'publication_date': publication_date,
             'seriess': seriess,
             'description': description,
+            
+        }
+
+        success = True
+        return success, encoding, elements
+    except Exception as e:
+        print ("ERROR: can`t open URL: %s with error %s" % (url, e))
+        return False, "", [e]
+    
+def fetch_author_data(url):
+    """Loads author data by URL;
+    Returns success, encoding, parsed elements"""
+    
+    
+    try:
+        resource = urlopen(url)
+        encoding = resource.headers.get_content_charset()
+        
+        soup = BeautifulSoup(resource,'html.parser')
+        
+        name, middle, surname = soup.title.string.partition(" ")
+        
+        name_raw = soup.find("h1", {"itemprop" : "name"}).string
+        name_orig_raw = name_raw[name_raw.find("(")+1 : name_raw.find(")")]
+        name_orig, middle, surname_orig = name_orig_raw.partition(" ")
+        
+        birth_date = soup.find("meta", {"itemprop" : "birthDate"}).findParent("td").text.strip()
+        
+        locale.setlocale(locale.LC_TIME, "ru_RU.utf8") #17 марта 1948 г.
+        d = {'января': 'январь', 'марта': 'март', 'декабря': 'декабрь'}
+        for k, v in d.items():
+            birth_date = birth_date.replace(k, v)
+        birth_date_obj = datetime.strptime(birth_date , '%d %B %Y г.')
+        
+        raw_biography = soup.find("div", {"class" : "person-info-bio"})
+        biography = ("".join(bio.text for bio in raw_biography.findChildren()))
+        
+        img = raw_biography.find("img")
+        photo = img['src']
+        
+        elements = {
+            'name': name,
+            'surname': surname,
+            'name_orig': name_orig,
+            'surname_orig': surname_orig,
+            'birth_date': birth_date_obj,
+            'biography': biography,
+            'photo':photo,
             
         }
 
