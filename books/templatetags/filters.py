f7e1179ee0eb5f187968cf360f1edb33097bcc8e
Nika <sleep.tight@post.com>
2018-07-17 21:01:24 +0200

    #32 filters cleanup

--- books/templatetags/filters.py
+++ books/templatetags/filters.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='get_book_action_status')
+@register.filter()
 def get_book_action_status(value, action):
     return "on" if value.shelf_set.filter(action=action) else "off"
