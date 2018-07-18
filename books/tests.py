9144e1b540036251c689445e19490ef8eebae28a
Nika <sleep.tight@post.com>
2018-07-18 21:05:22 +0200

    #27 books index view tests and fix

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from books.models import Book
+
+
+def create_book(title, title_orig):
+    """
+    Create a simple book with title and original title.
+    """
+    return Book.objects.create(title=title, title_orig=title_orig)
+
+
+class BooksIndexViewTests(TestCase):
+    def test_no_books(self):
+        """
+        If no books exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('books:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No books are available.")
+        self.assertQuerysetEqual(response.context['latest_books_list'], [])
+
+    def test_books_exist(self):
+        """
+        List of new added books is displayed on the index page.
+        """
+        create_book(title="Test", title_orig="Test orig")
+        response = self.client.get(reverse('books:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_books_list'],
+            ['<Book: Test>']
+        )
